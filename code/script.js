b287fb942aff6fca3e4bc2c3a1da0df0e65159cb
carolreedcruz <carolreedcruz@hotmail.com>
2023-12-05 12:11:29 +0100

    fetchdata done

--- code/script.js
+++ code/script.js
@@ -1,4 +1,44 @@
 console.log('Script is running')
+
+// fetch info
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    // Check if the response is successful
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the response as JSON
+  })
+  .then(data => {
+
+
+    data.forEach(data => {
+      const titleDiv = document.createElement('div');
+      titleDiv.classList.add('title');
+      titleDiv.textContent = data.title;
+
+      const bodyDiv = document.createElement('div');
+      bodyDiv.classList.add('description');
+      bodyDiv.textContent = data.body;
+
+      const accordion = document.getElementById('accordion');
+      const sectionDiv = document.createElement('div');
+      sectionDiv.appendChild(titleDiv);
+      sectionDiv.appendChild(bodyDiv);
+      accordion.appendChild(sectionDiv);
+
+      const titleElements = document.querySelectorAll(".title");
+      titleElements.forEach(element => {
+        element.addEventListener("click", toggle);
+      });
+    });
+  })
+
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+
+
 // A function that adds and remove the class "active" on the section you click on.
 function toggle(e) {
   const element = e.target;
@@ -6,9 +46,5 @@ function toggle(e) {
 }
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
-const section1Element = document.getElementById("section1");
-section1Element.addEventListener("click", toggle);
-const section2Element = document.getElementById("section2");
-section2Element.addEventListener("click", toggle);
-const section3Element = document.getElementById("section3");
-section3Element.addEventListener("click", toggle);
+
+
